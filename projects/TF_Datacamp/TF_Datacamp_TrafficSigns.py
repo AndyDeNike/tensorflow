6f343b7dd96ee0e5771f02383958249eb6fa4784
Andrew DeNike <denikeandrew@gmail.com>
2018-02-20 16:10:03 -0800

    Loaded Traffic Sign Data

---
+++ projects/TF_Datacamp/TF_Datacamp_TrafficSigns.py
@@ -0,0 +1,63 @@
+#Import 'tensorflow'
+import tensorflow as tf 
+
+#Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+#Multiply
+result = tf.multiply(x1, x2)
+
+#Initialize the Session
+#sess = tf.Session()
+
+#Print the result 
+#print(result)
+
+#Close the session 
+#sess.close()
+
+#Initialize Session and run 'result' (instead of previous commented code)
+with tf.Session() as sess:
+	output = sess.run(result)
+	print(output)
+
+
+#Loading and Exploring the Data:  Belgian Traffic Signs 
+
+#NOTE: The os.path module is always the path module suitable for the 
+#operating system Python is running on, and therefore usable for local 
+#paths.
+
+def load_data(data_directory):
+	#Using list comprehension, we iterate through files in 
+	#train_data_directory (Training folder) using 'd' and if it is a 
+	#directory, add it to the directories list
+	#Remember that each subdirectory represents a label.
+	directories = [d for d in os.listdir(data_directory)
+				   if os.path.isdir(os.path.join(data_directory, d))]
+	#Initialize two lists:
+	labels = []
+	images = []
+	#Iterate through directories and create 'file_names' list comp 
+	#which compiles all images with .ppm ending
+	#a for loop then appends all images/labels into respective lists
+	for d in directories:
+		label_directory = os.path.join(data_directory, d)
+		file_names = [os.path.join(label_directory, f)
+					  for f in os.listdir(label_directory)
+					  if f.endswith(".ppm")]
+		for f in file_names:
+			images.append(skimage.data.imread(f))
+			labels.append(int(d))
+	return images, labels 
+
+#ROOT_PATH is directory with all your training/test data 
+ROOT_PATH = "~/coding/tensorflow/projects/TF_Datacamp"
+#Join specific paths to ROOT_PATH with join()
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+#Load the data into the train_data_directory variable
+#The load_data() function gathers all subdirectories present in t_d_d
+images, labels = load_data(train_data_directory)
