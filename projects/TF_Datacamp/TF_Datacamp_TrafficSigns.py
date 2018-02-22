99263d3a507d3a17b08ecb966c35c2798a8df858
Andrew DeNike <denikeandrew@gmail.com>
2018-02-21 20:29:26 -0800

    Created Image Title Representation

--- projects/TF_Datacamp/TF_Datacamp_TrafficSigns.py
+++ projects/TF_Datacamp/TF_Datacamp_TrafficSigns.py
@@ -2,7 +2,7 @@
 import os
 import numpy as np
 import skimage
-from skimage import data
+from skimage import data, 
 import tensorflow as tf 
 import matplotlib.pyplot as plt
 
@@ -28,7 +28,7 @@ with tf.Session() as sess:
 	print(output)
 
 
-#Loading and Exploring the Data:  Belgian Traffic Signs 
+#LOADING AND EXPLORING THE DATA:  BELGIAN TRAFFIC SIGNS
 
 #NOTE: The os.path module is always the path module suitable for the 
 #operating system Python is running on, and therefore usable for local 
@@ -93,4 +93,58 @@ print(len(set(np.array(labels))))
 plt.hist(labels, 62)
 
 #Show the plot 
+plt.show()
+
+#VISUALIZING THE TRAFFIC SIGNS 
+
+#Determine the (random) indexes of the images that you want to see
+traffic_signs = [300, 2250, 3650, 4000]
+
+#Fill out the subplots with the random images that you defined
+for i in range(len(traffic_signs)):
+	plt.subplot(1, 4, i+1)
+	plt.axis('off')
+	plt.imshow(images[traffic_signs[i]])
+	plt.subplots_adjust(wspace=0.5)
+
+plt.show()
+
+#Fill out the subplots with the random images and add shape/min/max values
+for i in range(len(traffic_signs)):
+	plt.subplot(1, 4, i+1)
+	plt.axis('off')
+	plt.imshow(images[traffic_signs[i]])
+	plt.subplots_adjust(wspace=0.5)
+	plt.show()
+	print("shape: {0}, min: {1}, max: {2}".format(images[traffic_signs[i]].shape,
+												  images[traffic_signs[i]].min(),
+												  images[traffic_signs[i]].max()))
+
+
+#Get the unique labels 
+unique_labels = set(labels)
+
+#Initialize the figure
+plt.figure(figsize=(15,15))
+
+#Set a counter
+i = 1 
+
+#For each unique label,
+for label in unique_labels: 
+	#You pick the first image for each label 
+	image = images[labels.index(label)]
+	#Define 64 subplots 
+	plt.subplot(8, 8, i)
+	#plt.subplots_adjust(wspace=0.5)
+	#Don't include axes
+	plt.axis('off')
+	#Add a title to each subplot
+	plt.title("Label {0} ({1})".format(label, labels.count(label)))
+	#plt.title(label, y=3.08)
+	#Add 1 to the counter
+	i += 1 
+	#And you plot this first image 
+	plt.imshow(image)
+#Show the plot
 plt.show()
